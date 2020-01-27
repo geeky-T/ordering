0b073254e0137bbe3f422994c22567f969cbbac8
Tirth <pateltirth001@gmail.com>
2020-01-27 12:01:54 +0530

    attempted to integrate session management and implementation of login apis

--- app/Http/Controllers/UserLoginController.php
+++ app/Http/Controllers/UserLoginController.php
@@ -5,47 +5,55 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 
 
 class UserLoginController extends Controller
 {
     function login(Request $req)
     {
-        //print_r($req->input());
+        // check if user already logged in
+        if(
+            this.session()->has($req->input('email'))
+            &&
+            this.session()->get($req->input('email')) == $req->input('password')
+        ) return response('User Logged In!', 200);
 
-        $users = User::where('email', $req->email)->first();
+        // if not get user data from the database
+        $users = User::where('email', $req->input('email'))->where('password', $req->input('password'))->first();
+
+        // is user not found return bad request error
         if (is_null($users)) {
-            return response()->json(['error'=>'No email exists.Please register'], 401);
+            return response()->json(['error' => 'Invalid Email/Password. New User ? Please Register'], 401);
         }
-        else {
-            if(Hash::check($req->password,$users->password))
-            {
-                return response('Login Successful', 200);
-            }
-            else
-                {
-                    return response('Login Failed', 401);
-                }
-
+        // store session and respond with ok
+        else
+        {
+            $req->session()->put($req->input('email'), $req->input('password'));
+            return response('Login Successful', 200);
         }
     }
     function register(Request $req)
     {
-        $users = User::where('email', $req->email)->first();
+        if($req->session()->has('email')) return response()->json('', '200');
+        $users = User::where('email', $req->input('email')->first());
+
+        //checking user already exists
         if ($users) {
             return response()->json(['error'=>'Email already exists. Login please'], 401);
         }
+        // if not continue the registration process
         else
         {
             $user = new User();
             $user->name = $req->input('name');
             $user->email = $req->input('email');
-            $user->password = $password = Hash::make($req->password);;
-            //$user->password = $password = $req->input('password');
+//            $user->password = $password = Hash::make($req->input('password'));
+            $user->password = $password = $req->input('password');
             $user->save();
-            return response()->json(['message'=>'registration success']);
+            $req->session()->put('email', $req->input('email'));
+            return response('Registration Success', 201);
         }
-
     }
 
 
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        \Spatie\Cors\Cors::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
@@ -38,8 +39,11 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Session\Middleware\StartSession::class,
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'bindings',
         ],
     ];
 
